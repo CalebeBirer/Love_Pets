4fd8b2e2f8852677416161c87f290ffbb84779c6
Victor Jorge Sims <victorjsims@gmail.com>
2024-05-28 23:26:30 -0300

    update models

---
+++ agendamento/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from cliente.models import Users, Animal
+
+class Servico(models.Model):
+    nome = models.CharField(max_length=30)
+    descricao = models.CharField(max_length=150, blank=True, null=True)
+
+    def __str__(self):
+        return self.nome
+
+class Agendamento(models.Model):
+    data = models.DateField()
+    horario = models.TimeField()
+    observacao = models.CharField(max_length=150, blank=True, null=True)
+    finalizado = models.BooleanField(default=False)
+    id_animal = models.ForeignKey(Animal, on_delete=models.PROTECT)
+    id_cliente = models.ForeignKey(Users, on_delete=models.PROTECT)
+    id_servico = models.ForeignKey(Servico, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.data} {self.horario} - {self.id_servico.nome}"
--- cliente/models.py
+++ cliente/models.py
@@ -2,7 +2,43 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Users(AbstractUser):
-    choices_cargo = (('W', 'Owner'),
-                     ('C', 'Client'))
+    choices_cargo = (('W', 'Owner'), ('C', 'Client'))
     cargo = models.CharField(max_length=1, choices=choices_cargo)
-    
+
+
+class Client(models.Model):
+    nome = models.CharField(max_length=20)
+    sobrenome = models.CharField(max_length=50)
+    cpf = models.CharField(max_length=11)
+    telefone = models.CharField(max_length=11)
+    choices_sexo = (('M', 'Masculino'), ('F', 'Feminino'))
+    sexo = models.CharField(max_length=1, choices=choices_sexo)
+    cep = models.CharField(max_length=11)
+    choices_estado = (
+        ('AC', 'Acre'), ('AL', 'Alagoas'), ('AP', 'Amapá'), ('AM', 'Amazonas'), ('BA', 'Bahia'),
+        ('CE', 'Ceará'), ('ES', 'Espírito Santo'), ('GO', 'Goiás'), ('MA', 'Maranhão'), ('MT', 'Mato Grosso'),
+        ('MS', 'Mato Grosso do Sul'), ('MG', 'Minas Gerais'), ('PA', 'Pará'), ('PB', 'Paraíba'), ('PR', 'Paraná'),
+        ('PE', 'Pernambuco'), ('PI', 'Piauí'), ('RJ', 'Rio de Janeiro'), ('RN', 'Rio Grande do Norte'),
+        ('RS', 'Rio Grande do Sul'), ('RO', 'Rondônia'), ('RR', 'Roraima'), ('SC', 'Santa Catarina'),
+        ('SP', 'São Paulo'), ('SE', 'Sergipe'), ('TO', 'Tocantins'), ('DF', 'Distrito Federal')
+    )
+    estado = models.CharField(max_length=2, choices=choices_estado, default='SP')
+    bairro = models.CharField(max_length=30)
+    numero = models.CharField(max_length=6)  # Utilize CharField para preservar zeros à esquerda
+    complemento = models.CharField(max_length=30, blank=True, null=True)
+    id_user = models.ForeignKey(Users, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+
+
+class Animal(models.Model):
+    nome = models.CharField(max_length=50)
+    raca = models.CharField(max_length=20)
+    tipo_pelo = models.BooleanField(default=False)  # longo → True / curto → False
+    choice_porte = (('P', 'Pequeno'), ('M', 'Médio'), ('G', 'Grande'))
+    porte = models.CharField(max_length=1, choices=choice_porte)
+    id_client = models.ForeignKey(Client, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
