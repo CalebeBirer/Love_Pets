3b68e940f137d25b509a55e5d0e78387e5655320
Victor Jorge Sims <victorjsims@gmail.com>
2024-05-28 23:32:57 -0300

    update urls

--- cliente/urls.py
+++ cliente/urls.py
@@ -1,11 +1,9 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.inicio, name="inicio"),
     path('login/', views.login, name="login"),
-    path('logout', views.logout, name="logout"),
-    path('agenda/', views.agenda, name="agenda"),
+    path('logout', views.logout, name="logout"),    
     path('register_client/', views.register_client, name="register_client")
 ]
--- love_pets/urls.py
+++ love_pets/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls), 
-    path('', include('cliente.urls')),
-    path('inicio/', include('cliente.urls')),         
+    path('', include('cliente.urls')),    
+    path('agenda/', include('agendamento.urls')),
 ]
